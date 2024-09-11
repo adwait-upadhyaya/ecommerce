973377e74990aba27b50e8307c06115453a7902c
adwaitupadhyaya <adwait627@gmail.com>
2024-09-11 18:00:51 +0545

    persist redux

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { RouterProvider } from "react-router-dom";
 import "./styles/styles.scss";
 import { router } from "./router";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <>
       <Provider store={store}>
-        <RouterProvider router={router} />
+        <PersistGate loading={null} persistor={persistor}>
+          <RouterProvider router={router} />
+        </PersistGate>
       </Provider>
     </>
   );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productApi } from "../services/products";
 import cartReducer from "./slices/cartSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartReducer);
 
 export const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: persistedReducer,
     [productApi.reducerPath]: productApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(productApi.middleware),
 });
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
